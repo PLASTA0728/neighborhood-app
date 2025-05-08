a540c3a366c6e190940cf3083f784947e5abf1fe
StephYSN <3011316681@qq.com>
2025-05-08 18:55:41 +0800

    add error message page

--- nextjs/nextjs-dashboard/app/lib/actions.ts
+++ nextjs/nextjs-dashboard/app/lib/actions.ts
@@ -59,6 +59,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
+    // throw new Error('Failed to Delete Invoice'); to throw an error
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
   }
