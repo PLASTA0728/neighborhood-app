14b5069e3edf08974107de8410e847d4f401d4ad
StephYSN <3011316681@qq.com>
2025-05-14 16:37:14 +0800

    updated front page styling

--- pathtogether/app/page.tsx
+++ pathtogether/app/page.tsx
@@ -34,16 +34,21 @@ function App() {
 return (
   <div className="static">
     <div className="absolute -z-10 w-full h-full" id="app" ref={appRef} data-scroll-container></div>
-    <div className="absolute bg-black z-0 w-full h-full opacity-50"></div>
-    <div className={`${jost.className} z-10 absolute top-1/2 left-1/7`}>
+    <div className="absolute bg-black z-0 w-full h-full opacity-65"></div>
+    <div className={`${jost.className} z-20 absolute top-1/2 left-1/7`}>
       <div className="text-6xl font-normal">PathTogether</div>
       <div className="text-3xl font-extralight">one map, every destination</div>
     </div>
-    <div className="absolute top-1/2 right-1/5">
-      <input className="flex placeholder:text-gray-500 bg-black px-4 py-4 w-75 relative left-0 rounded-md" placeholder="enter your session code" type="string" name="enter-session-code" />
-      <Link href={"/new-session"} className="relative left-4 top-2">don't have one? generate now!</Link>
+    <div className="absolute top-1/2 left-3/5">
+    <div className="flex">
+      <input className="z-20 placeholder:text-gray-500 bg-black px-4 py-4 w-75 relative left-0 rounded-md" placeholder="enter your session code" type="string" name="enter-session-code" />
+      <Link href={"/edit-session"}>
+      <svg xmlns="http://www.w3.org/2000/svg" className="z-40 relative right-10 top-4 fill-none stroke-white stroke-2 size-6"><path stroke-linecap="round" stroke-linejoin="round" d="m7.49 12-3.75 3.75m0 0 3.75 3.75m-3.75-3.75h16.5V4.499" /></svg>
+      </Link>
     </div>
-  {/* <iframe src="https://snazzymaps.com/embed/708506" ></iframe> */}
+    <Link href={"/new-session"} className="relative left-4 top-2">don't have one? generate now!</Link>
+    </div>
+  {/* <iframe src="https://snazzymaps.com/embed/708506" className="absolute w-full h-full z-10 opacity-20"></iframe> */}
 </div>
 );
 }
