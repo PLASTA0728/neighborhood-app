d5ba41db1032804dcc47f1e3cfc09b45d1319c5b
StephYSN <3011316681@qq.com>
2025-06-07 14:49:43 +0700

    added info on how this app works and how to get started

--- pathtogether/app/new-session/page.tsx
+++ pathtogether/app/new-session/page.tsx
@@ -7,6 +7,7 @@ import { cousine } from "@/ui/fonts";
 import { useSessionManager } from "@/hooks/useSessionManager";
 import { getFieldsForTemplate } from '@/lib/getFieldsForTemplate'
 import FormInput from "@/components/FormInput";
+import Link from "next/link";
 // import { generateUniqueSessionNo } from "@/app/lib/session-generator";
 
 
@@ -126,9 +127,21 @@ export default function NewSession() {
             share my session!
           </Button>
         </div>
-        <div className="flex-1 bg-gray-100 flex items-center justify-center p-10">
+        <div className="flex-1 bg-gray-100 flex justify-center p-10">
           {/* You can later replace this with a real map component */}
-          <div className="text-gray-600 text-xl">walkthrough on how to create the map and promotion of different styles will be displayed here</div>
+          {/* <div className="text-gray-600 text-xl">walkthrough on how to create the map and promotion of different styles will be displayed here</div> */}
+          <div className="relative text-gray-600 grid gap-2">
+            <h2 className="text-gray-800 text-xl">to get started</h2>
+            <p>besides the <Link href="/" className="text-blue-400">landing page</Link>, there are two main pages: <Link href="/new-session">create new session</Link> & edit session (which you <span className="inline bold text-red-500">can't</span> get access to before having a session code).</p>
+            <p>you are currently on the <Link href="/new-session" className="text-blue-400">create session page</Link>. after you create a session, you can click on the "share my session" button below to get your session code on a pop-up. Your session code should be in the format of alphanumerical digits of length 8.</p>
+            <p>after you get your session code, you can copy paste the session code to your friends. after they enter the session code in the input box on the landing page and press enter, they will be redirected to the edit page of your customized session to enter their information.</p>
+            <h2 className="text-gray-800 text-xl">how to design a customized session?</h2>
+            <p>after people enter the session code and get access to the session, they will see default basic entries including `name`, `age`, `contact`, `role`, and `location`; if you enter any customized fields on this create new session page, they will also be able to see the entries under the default entries.</p>
+            <p>people may have different demands over what other entries they would like to add:</p>
+            <p className="text-gray-500 italic text-center">i.e. for high school seniors that are about to graduate, they may want to add their `committed college` as one customized field. </p>
+            <p>you are allowed to control in which type should the answers be to avoid unhinged answers, for example, if you want others to enter their birthday, make them in the same format by selecting the `Date` type in the dropdown menu. click the <Plus className="inline size-4 relative bottom-0.5"/> button to confirm adding the field, and click the <Minus className="inline size-4 relative bottom-0.5 text-white bg-red-500 text-center rounded-full"/> to delete customized fields that you don't want to add anymore.</p>
+            <p>"templates" have been added as a way to quickly get access to some of the entries assumed to be used, and you can select them in the dropdown menu to see if any of which fits your need. it is also a good way to get a sense of what customized entries <span className="inline italic">you</span> want to implement!</p>
+          </div>
         </div>
       </div>
       {showPopup && (
