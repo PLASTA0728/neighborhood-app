7829b4a7676f05152e5aadae1573a16f7588b4d2
StephYSN <3011316681@qq.com>
2025-06-19 08:45:10 +0800

    good bye to my test folders, now panel updated and user map is now used

--- pathtogether/components/Panel.tsx
+++ pathtogether/components/Panel.tsx
@@ -4,6 +4,7 @@
 import { Button } from '@/ui/button';
 import Card from './Card';
 import type { IUser } from '@/utils/types';
+import UserMap from './UserMap';
 
 type Props = {
     sessionNo: string;
@@ -13,22 +14,12 @@ type Props = {
 
 export default function Panel({ sessionNo, users, refreshUsers }: Props) {
     return (
-        <div className='flex-1 bg-gray-100 p-4 relative min-h-screen'>
+        <div className='flex-1 bg-gray-100 relative min-h-screen'>
             <Button className='absolute top-4 right-4 z-30' onClick={refreshUsers}>
                 refresh
             </Button>
             <div className='w-full flex justify-center'>
-                <div className='mt-16 flex flex-wrap gap-4 justify-start'>
-                    {users.length > 0 ? (
-                        users.map((user, i) => (
-                            <Card key={i} sessionNo={sessionNo} user={user} refreshUsers={refreshUsers} />
-                        ))
-                    ) : (
-                        <p className='text-sm text-gray-400 text-center'>
-                            no users. <span className='inline italic'>yet?</span>
-                        </p>
-                    )}
-                </div>
+                <UserMap sessionNo={sessionNo} users={users} refreshUsers={refreshUsers}/>
             </div>
         </div>
     );
